4790b1f84fdc97f4c6ca04b45947c99278fd164a
Erik Thomsen <ethomsen86@gmail.com>
2021-03-02 21:05:19 -0500

    Testing with sleep command

--- ansible-vpn/ansible-action/entrypoint.sh
+++ ansible-vpn/ansible-action/entrypoint.sh
@@ -2,5 +2,7 @@
 
 openvpn --config grosso_${OFFICE}.ovpn --auth-user-pass auth.txt --daemon
 
+sleep 20
+
 cd mount/
 ansible-playbook -i hosts ${PHASE}-computers.yml --vault-password-file vault_password
