92daa2df76bfa0d5f4fc2bf55c68604edf160443
Erik Thomsen <ethomsen86@gmail.com>
2021-03-02 21:36:25 -0500

    Added debug info

--- ansible-vpn/ansible-action/entrypoint.sh
+++ ansible-vpn/ansible-action/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh -l
 
+# Debug start
+ls -ltrh
+echo "openvpn --config grosso_${OFFICE}.ovpn --auth-user-pass auth.txt --daemon"
+# Debug end
 openvpn --config grosso_${OFFICE}.ovpn --auth-user-pass auth.txt --daemon
-
-sleep 20
 
 cd mount/
 ansible-playbook -i hosts ${PHASE}-computers.yml --vault-password-file vault_password
