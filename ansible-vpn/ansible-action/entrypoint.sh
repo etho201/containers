8224334a7f7a0a66683898a72745398cfd4c6a24
Erik Thomsen <ethomsen86@gmail.com>
2021-03-03 22:25:17 -0500

    Removed secrets from docker build process

--- ansible-vpn/ansible-action/entrypoint.sh
+++ ansible-vpn/ansible-action/entrypoint.sh
@@ -3,8 +3,8 @@
 # Debug start
 ls -ltrh
 echo "openvpn --config grosso_${OFFICE}.ovpn --auth-user-pass auth.txt --daemon"
+echo "ansible-playbook -i hosts ${PHASE}-computers.yml --vault-password-file vault_password"
 # Debug end
-openvpn --config grosso_${OFFICE}.ovpn --auth-user-pass auth.txt --daemon
 
-cd mount/
+openvpn --config grosso_${OFFICE}.ovpn --auth-user-pass auth.txt --daemon
 ansible-playbook -i hosts ${PHASE}-computers.yml --vault-password-file vault_password
