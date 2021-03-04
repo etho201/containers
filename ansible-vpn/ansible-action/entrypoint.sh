4a21e96f71b7ed6053b0a5a947f40e09da45a7da
Erik Thomsen <ethomsen86@gmail.com>
2021-03-03 22:28:31 -0500

    Updated auth file name

--- ansible-vpn/ansible-action/entrypoint.sh
+++ ansible-vpn/ansible-action/entrypoint.sh
@@ -6,5 +6,5 @@ echo "openvpn --config grosso_${OFFICE}.ovpn --auth-user-pass auth.txt --daemon"
 echo "ansible-playbook -i hosts ${PHASE}-computers.yml --vault-password-file vault_password"
 # Debug end
 
-openvpn --config grosso_${OFFICE}.ovpn --auth-user-pass auth.txt --daemon
+openvpn --config grosso_${OFFICE}.ovpn --auth-user-pass ovpn_auth.txt --daemon
 ansible-playbook -i hosts ${PHASE}-computers.yml --vault-password-file vault_password
